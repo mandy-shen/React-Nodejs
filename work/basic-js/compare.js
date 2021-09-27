de0abec24536649f914d94edb15bd75ff111209d
mandy-shen <shen.mo@northeastern.edu>
2021-09-26 21:44:36 -0700

    motify some 'let' to 'const'

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -6,20 +6,20 @@ function compare( word, guess ) {  // DO NOT MODIFY
   if (!word || !guess)
     return 0;
 
-  let newWord = word.toLowerCase();
-  let newGuess = guess.toLowerCase();
+  const newWord = word.toLowerCase();
+  const newGuess = guess.toLowerCase();
 
-  let arr = new Array(256);
+  const arr = new Array(256);
   arr.fill(0);
 
-  for (let i in newWord) {
-    let code = newWord.charCodeAt(i);
+  for (const i in newWord) {
+    const code = newWord.charCodeAt(i);
     arr[code]++;
   }
 
   let cnt = 0;
-  for (let i in newGuess) {
-    let code = newGuess.charCodeAt(i);
+  for (const i in newGuess) {
+    const code = newGuess.charCodeAt(i);
     if (arr[code] > 0) {
       cnt++;
       arr[code]--;
