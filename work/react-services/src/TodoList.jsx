8daaf0afb123d24e9786b6f701d9f8481aa10c51
mandy-shen <shen.mo@northeastern.edu>
2021-11-21 18:57:24 -0800

    commit login, todolist

--- work/react-services/src/TodoList.jsx
+++ work/react-services/src/TodoList.jsx
@@ -2,17 +2,13 @@ function TodoList({todos}) {
     console.log("todos="+todos);
 
 
-    const list = Object.values(todos).map( todo => `
-      <li className="todo">
-        <input type="checkbox">
-        <span>{todo.task}</span>
-        <button>Delete</button>
-      </li>
-    `).join('');
+    const list = Object.values(todos).map( todo => (
+      <li className="todo">{todo.task}</li>
+    ));
 
 
 
-    return (<ul className="todos">456_{list}</ul>);
+    return (<ul className="todos">{list}</ul>);
 }
 
 export default TodoList;
