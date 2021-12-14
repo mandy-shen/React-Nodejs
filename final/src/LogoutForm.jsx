1da6dd34b2c7962f7b988f570345201a10e71aa2
mandy-shen <shen.mo@northeastern.edu>
2021-12-13 18:02:42 -0800

    fixed username bug on Logoutform.jsx when re-rendering page

--- final/src/LogoutForm.jsx
+++ final/src/LogoutForm.jsx
@@ -4,10 +4,12 @@ import {useContext} from "react";
 const LogoutForm = ({username}) => {
     const { onLogout } = useContext(TodoContext);
 
+    const name = Object.values(username);
+
     return (
         <div className="logout">
             <form>
-                <span>Login Username: {username}</span><br/>
+                <span>Login Username: {name}</span><br/>
                 <button type="submit" onClick={() => onLogout()} >Logout</button>
             </form>
         </div>
